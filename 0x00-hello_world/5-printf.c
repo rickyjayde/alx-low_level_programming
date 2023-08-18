f3f972e5b5fb9b1b72a577a0b4210618dd59b951
rickyjayde <jadewmwangi@gmail.com>
2023-08-18 06:15:02 -0700

    made

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+ **main-Entry point
+ *
+ * *Return:Always 0 (Success_
+ */
+int main(void)
+{
+	printf("with proper grammmar,but the outcome is  piece of art,\n";
+			return (0);
+}
