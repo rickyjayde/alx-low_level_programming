dc66fb0f93930deeb4a830e78d691783c9823911
rickyjayde <jadewmwangi@gmail.com>
2023-08-23 03:47:28 -0700

    phew

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,11 @@
 #include <stdio.h>
+/**
+ * main - Entry point of the program
+ *
+ * This function prints the string "_putchar" to the standard output.
+ *
+ * Return: Always 0 (Success)
+ */
 
 int main(void)
 {
