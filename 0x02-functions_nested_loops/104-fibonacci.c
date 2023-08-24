039ccf5d5079b39b0bee6864f27296866a52eba1
rickyjayde <jadewmwangi@gmail.com>
2023-08-24 04:57:14 -0700

    gtyr

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -4,10 +4,10 @@ int main(void)
 {
     int n = 98; /* Number of Fibonacci numbers to generate */
     unsigned int fib1 = 1, fib2 = 2, nextFib;
+    int i; /* Declare 'i' here */
 
     printf("%u, %u", fib1, fib2);
 
-    int i;
     for (i = 3; i <= n; i++)
     {
         nextFib = fib1 + fib2;
