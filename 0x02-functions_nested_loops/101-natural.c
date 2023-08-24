d5d9c2b80b4423a278c513d2c771bb29a5e768dd
rickyjayde <jadewmwangi@gmail.com>
2023-08-24 03:58:00 -0700

    betty' git push

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -3,25 +3,23 @@
 /**
  * main - Entry point
  *
- * Description: Computes and prints the sum of multiples of 3 or 5 below 1024
- *
- * Return: Always 0 (Success)
+ * Return: Always 0
  */
 int main(void)
 {
-        int sum = 0;
-        int i;
+    int sum = 0;
+    int i;
 
-        for (i = 3; i < 1024; i++)
+    for (i = 3; i < 1024; i++)
+    {
+        if (i % 3 == 0 || i % 5 == 0)
         {
-                if (i % 3 == 0 || i % 5 == 0)
-                {
-                        sum += i;
-                }
+            sum += i;
         }
+    }
 
-        printf("Sum of multiples of 3 or 5 below 1024: %d\n", sum);
+    printf("Sum of multiples of 3 or 5 below 1024: %d\n", sum);
 
-        return (0);
+    return 0;
 }
 
