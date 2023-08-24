050f1a023ad2de50e7fb3dd833e8432612b0fd0f
rickyjayde <jadewmwangi@gmail.com>
2023-08-24 03:47:02 -0700

    pur

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int sum = 0;
+	for (int i = 3; i < 1024; i++)
+    {
+        if (i % 3 == 0 || i % 5 == 0)
+        {
+            sum += i;
+        }
+    }
+
+    printf("Sum of multiples of 3 or 5 below 1024: %d\n", sum);
+
+    return (0);
+}
