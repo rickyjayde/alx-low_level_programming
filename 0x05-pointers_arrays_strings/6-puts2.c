407f07ce2a4bcad57e1d2e6ab3fd6b507ca5397a
rickyjayde <jadewmwangi@gmail.com>
2023-08-29 07:24:28 -0700

    jhennesy

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stddef.h>
+
+void puts2(char *str) {
+  int i = 0;
+  while (str[i] != '\0') {
+    if (i % 2 == 0)
+      putchar(str[i]);
+    i++;
+  }
+  putchar('\n');
+}
