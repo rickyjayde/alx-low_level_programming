ea9dd1fc0f6dd82bf1af9781a31cf6e73012de0f
rickyjayde <jadewmwangi@gmail.com>
2023-08-29 08:18:13 -0700

    afred

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -5,13 +5,15 @@ void puts_half(char *str) {
     int length = strlen(str);
     int start_index;
 
+    int i; /* Declare the loop counter here */
+
     if (length % 2 == 0) {
         start_index = length / 2;
     } else {
         start_index = (length - 1) / 2;
     }
 
-    for (int i = start_index; i < length; i++) {
+    for (i = start_index; i < length; i++) {
         putchar(str[i]);
     }
     putchar('\n');
